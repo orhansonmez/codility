0401179271f12b1fa0ebd332d870ab962297decd
Orhan Sönmez <orhansonmez@gmail.com>
2022-11-02 16:21:55 +0100

    Create 06 Distinct.py

---
+++ lessons/06 Distinct.py
@@ -0,0 +1,13 @@
+def solution(A):
+    
+    if A == []:
+        return 0
+
+    A.sort()
+
+    unique = 1
+    for i in range(len(A) - 1):
+        if A[i] != A[i + 1]:
+            unique += 1
+
+    return unique
