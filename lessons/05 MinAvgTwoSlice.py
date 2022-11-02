302ee8e8250d10e697b2f8539d1c1754913a82ab
Orhan Sönmez <orhansonmez@gmail.com>
2022-11-02 16:00:32 +0100

    MinAvgTwoSlice Efficient Solution

--- lessons/05 MinAvgTwoSlice.py
+++ lessons/05 MinAvgTwoSlice.py
@@ -18,3 +18,30 @@ def solution_not_efficient(A):
                 min_avg_index = p
 
     return min_avg_index
+
+
+def solution(A):
+
+    N = len(A)
+    prefix_sums = [0] * (N + 1)
+    for i in range(N):
+        prefix_sums[i + 1] = prefix_sums[i] + A[i]
+
+    min_avg = 10001
+    min_avg_index = -1
+
+    # Slices with 2-length
+    for p in range(N - 1):
+        avg = (prefix_sums[p + 2] - prefix_sums[p]) / 2
+        if avg < min_avg:
+            min_avg = avg
+            min_avg_index = p
+
+    # Slices with 3-length
+    for p in range(N - 2):
+        avg = (prefix_sums[p + 3] - prefix_sums[p]) / 3
+        if avg < min_avg:
+            min_avg = avg
+            min_avg_index = p
+
+    return min_avg_index
