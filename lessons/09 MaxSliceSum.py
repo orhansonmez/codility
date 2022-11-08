85c28768b38c4f85e453a627bb740d3263a8c24e
Orhan Sönmez <orhansonmez@gmail.com>
2022-11-08 17:56:47 +0200

    Update 09 MaxSliceSum.py

--- lessons/09 MaxSliceSum.py
+++ lessons/09 MaxSliceSum.py
@@ -1,15 +1,8 @@
 def solution(A):
 
-    max_value = -1e12
+    max_slice, max_sum = -1e12, 0
     for a in A:
-        max_value = max(max_value, a)
-
-    if max_value <= 0:
-        return max_value
-
-    max_slice, max_sum = 0, 0
-    for a in A:
-        max_sum = max(0, max_sum + a)
+        max_sum = max(a, max_sum + a)
         max_slice = max(max_slice, max_sum)
 
     return max_slice
