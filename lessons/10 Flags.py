bf48f6a3d2b26770443faf0723db6afef7ffa770
Orhan Sönmez <orhansonmez@gmail.com>
2022-11-21 18:52:42 +0100

    10 Flags Not Efficient Solution

---
+++ lessons/10 Flags.py
@@ -0,0 +1,36 @@
+def solution_not_efficient(A):
+
+    N = len(A)
+    if N < 3:
+        return 0
+
+    peaks = []
+    for i in range(1, N - 1):
+        if A[i] > A[i - 1] and A[i] > A[i + 1]:
+            peaks.append(i)
+
+    P = len(peaks)
+    if not peaks:
+        return 0
+
+    diff = []
+    for i in range(P - 1):
+        diff.append(peaks[i + 1] - peaks[i])
+
+    max_flags = 1
+    for K in range(2, P + 1):
+
+        flags, distance = 1, 0
+        for d in diff:
+            if distance + d >= K:
+                distance = 0
+                flags += 1
+            else:
+                distance += d
+
+        if flags >= K:
+            max_flags = K
+        else:
+            break
+
+    return max_flags
