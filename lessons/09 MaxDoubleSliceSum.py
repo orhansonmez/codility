80db7a2fe19a288fe253ea871c118d47df40cbdf
Orhan Sönmez <orhansonmez@gmail.com>
2022-11-21 12:19:07 +0100

    MaxDoubleSliceSum Efficient Solution

--- lessons/09 MaxDoubleSliceSum.py
+++ lessons/09 MaxDoubleSliceSum.py
@@ -16,3 +16,21 @@ def solution_not_efficient(A):
                     max_double_slice = double_slice
 
     return max_double_slice
+
+def solution(A):
+
+    N = len(A)
+    forward = [0] * N
+    backward = [0] * N
+
+    for i in range(1, N - 2):
+        forward[i + 1] = max(0, forward[i] + A[i])
+
+    for i in range(N - 2, 1, -1):
+        backward[i - 1] = max(0, backward[i] + A[i])
+
+    max_sum = 0
+    for i in range(1, N - 1):
+        max_sum = max(max_sum, forward[i] + backward[i])
+
+    return max_sum
