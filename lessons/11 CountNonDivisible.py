8c8d34750e473e688adc9d425d6f992ee28f7e1b
Orhan Sönmez <orhansonmez@gmail.com>
2022-11-22 01:15:41 +0100

    11 CountNonDivisible - Solution

--- lessons/11 CountNonDivisible.py
+++ lessons/11 CountNonDivisible.py
@@ -9,3 +9,27 @@ def solution_not_efficient(A):
                 non_divisors[i] += 1
 
     return non_divisors
+
+
+def solution(A):
+
+    N = len(A)
+    max_value = max(A)
+
+    counts = [0] * (max_value + 1)
+    for a in A:
+        counts[a] += 1
+
+    non_divisors = [N] * (max_value + 1)
+
+    for i in range(1, max_value + 1):
+        j = i
+        while j <= max_value:
+            non_divisors[j] -= counts[i]
+            j += i
+
+    output = []
+    for a in A:
+        output.append(non_divisors[a])
+
+    return output
